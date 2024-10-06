6494fc5c23bd93dede1618890783f8dda249cfc8
georgeckito <gigickitishvili@gmail.com>
2024-10-06 10:07:10 -0700

    davaleba 2

--- davaleba2.cs
+++ davaleba2.cs
@@ -10,7 +10,17 @@ namespace _10oqtomberi
     {
         public void Execute()
         {
-
+            double num;
+            Console.WriteLine("Enter your number:");
+            num = Convert.ToDouble(Console.ReadLine());
+            int mteli = Convert.ToInt32(num);
+            if (mteli < 0) {
+                mteli++;
+            }
+            Console.WriteLine($"Answer: {num - mteli}");
+            if ((num - mteli) == 0) {
+                Console.WriteLine("savaraudod mdzime daweret wertilis magivrad (naxevari saati ver vugebdi nuli rato iyo)");
+            }
         }
     }
 }
