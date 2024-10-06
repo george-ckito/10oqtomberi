e9664d25047e4d9daa46e1a8e407a4a2ce19f2c8
georgeckito <gigickitishvili@gmail.com>
2024-10-06 09:40:50 -0700

    davaleba 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _10oqtomberi;
+
+Console.WriteLine("Which davaleba would you like to run? 1/2/3/4/bonus");
+string choice = Console.ReadLine();
+if(Convert.ToInt32(choice) == 1)
+{
+    Davaleba1 davaleba = new Davaleba1();
+    davaleba.Execute();
+} else if (Convert.ToInt32(choice) == 2)
+{
+    Davaleba2 davaleba = new Davaleba2();
+    davaleba.Execute();
+}
---
+++ davaleba1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10oqtomberi
+{
+    internal class Davaleba1
+    {
+        public void Execute() {
+            int a, b;
+            bool withoutAdd;
+            Console.WriteLine("Enter the first number:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Without additional variable? y/n");
+            withoutAdd = Console.ReadLine() == "y";
+            if (withoutAdd)
+            {
+                a = a + b;
+                b = a - b;
+                a = a - b;
+            }
+            else
+            {
+                int c;
+                c = a;
+                a = b;
+                b = c;
+            };
+            Console.WriteLine($"Ints Switched: a = {a}; b = {b}");
+        }
+    }
+}
